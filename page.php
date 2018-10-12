f25af6b851a65c71e4eda6eb72e46bcd3b234406
David Kinloch <dkinloch@gmail.com>
2018-10-12 15:32:46 +0200

    Renamed structure

---
+++ page.php
@@ -0,0 +1,118 @@
+<?php include 'header.php'; ?>
+      
+<div class="section section--hero">
+    <div class="section__inner wrapper">
+        <div class="caption">
+            <img src="assets/build/tt-logo-white.svg">
+        </div>
+    </div>
+</div>
+
+<div class="section section--text">
+    <div class="section__inner">
+        <div class="caption">
+            <h2 class="title">As Simple as Sketching on a Pad</h2>
+            <p>With an interface as simple as sketching on a pad, Tommy Trinder (cockney rhyming slang for ‘window’) uses unique Framepoint® web technology to convert touch-screen input into accurate, photorealistic renditions of windows and doors. Once drawn, overlay your designs onto your client’s home using our ground breaking Makeover tool. All the while products are priced in real time and orders may be placed with multiple suppliers with one click.</p>
+        </div>
+    </div>
+</div>
+
+<div class="section section--split">
+    <div class="section__inner">
+        <img src="assets/build/virtual-showroom.jpg" alt="Tommy Trinder - Virtual Showroom" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">Your Virtual Showroom</h3>
+                <p>Engage clients in a collaborative design process as you sketch out ideas together. Point, draw, and touch to adjust sizes, profiles, colours, glass, glazing features, handles &amp; hardware. See your designs come to life in full photographic quality.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--split section--inverse">
+    <div class="section__inner">
+        <img src="assets/build/many-suppliers.jpg" alt="Tommy Tinder - Many Suppliers" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">Many Suppliers, One Quote.</h3>
+                <p>PVCu Sash Windows, a timber front door, an aluminium bi-fold and PVCu casements? No problem. Combine products from multiple suppliers in one quote, safe in the knowledge that the dataset is current and accurate. And when you are ready, place orders on multiple fabricators with one click.</p>
+            </div>
+        </div>
+    </div>
+    
+</div>
+
+<div class="section section--split">
+    <div class="section__inner">
+        <img src="assets/src/price-p.png" alt="Tommy Trinder - Price in Materials" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">Price in PVCu, Aluminium, Timber? </h3>
+                <p>Clone your quote from one material to another with one click to provide optional costings to your client in a variety of materials. Hours of work, reduced to seconds.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--split section--inverse">
+    <div class="section__inner">
+        <img src="assets/build/overlay-v2.png" alt="The Visualisator" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">Overlay your designs onto your client’s home</h3>
+                <p>Take a photo on your iPAD and overlay your designs onto your clients home – quick, effortless intuitive. Drag and skew images to create life-like Makeovers. Change the quote and instantly see the effect in situ.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--split">
+    <div class="section__inner">
+        <img src="assets/build/ipad-float.png" alt="dummy image" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">Take the order, transact the deposit.</h3>
+                <p>Close the order by generating a contract, collecting your clients signature and  transacting deposit payments live via a secure payment portal. </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--split section--inverse">
+    <div class="section__inner">
+        <img src="assets/build/docs.png" alt="dummy image" /> 
+    </div>
+    <div class="section__inner">
+        <div class="caption">
+            <div class="caption__inner">
+                <h3 class="title">First Class Documentation for Clients &amp; Suppliers</h3>
+                <p>Impress and minimise error with a quotation showing inside and outside, photorealistic renditions in full colour. Customise with your brand, your payment terms, your lead times, your salesman’s name. Deliver clean orders to suppliers, specifically tailored to the needs of their order processing teams.</p><br>
+                <p><a class="button button--primary" href="assets/build/Sample_Special_RAL_Windows.pdf"><i class="fa fa-pdf"></i>Download Sample Order PDF</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--text">
+    <div class="section__inner">
+        <div class="caption">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id neque sapien. Sed at lectus tristique, elementum tortor et, pretium risus. Proin sed faucibus metus, non fringilla turpis. Vivamus ac semper nisi.</p><br>
+            <p><a href="#" class="button button--primary">Get More Details</a></p>
+        </div>
+    </div>
+</div>
+
+              
+
+<?php include 'footer.php'; ?>
+
