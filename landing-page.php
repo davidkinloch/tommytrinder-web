c4924a767809a16631918b3e3290be7e1265278a
David Kinloch <dkinloch@gmail.com>
2018-11-26 11:00:06 +0100

    Landign page marigns

--- landing-page.php
+++ landing-page.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
       
-<div class="section section--vimeo" style="max-width: 1680px;margin: 80px auto 0;">
+<div class="section section--vimeo" style="max-width: 1680px;">
     <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/281562791?autoplay=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 </div>
 
