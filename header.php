d8c76e03972fd9decc17d7bbeb01f5bca42ddba1
David Kinloch <dkinloch@gmail.com>
2018-12-12 12:46:54 +0100

    Fav and og meta deets added

--- header.php
+++ header.php
@@ -6,14 +6,19 @@
         <title>Frontend Templates</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- https://realfavicongenerator.net -->
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#333333">
+        <meta name="msapplication-TileColor" content="#333333">
         <meta name="theme-color" content="#ffffff">
+        <meta property="og:image:width" content="1280">
+        <meta property="og:image:height" content="670">
+        <meta property="og:title" content="Tommy Trinder Ltd">
+        <meta property="og:description" content="Using unique Framepoint&reg; web technology convert touch-screen input into accurate, photorealistic renditions of windows and doors...">
+        <meta property="og:url" content="https://www.tommytrinder.com/">
+        <meta property="og:image" content="https://www.tommytrinder.com/assets/build/og-image.jpg">
         <link rel="stylesheet" type="text/css" href="css/main.css" />
         
     </head>
@@ -24,8 +29,6 @@
                 <a href="mailto:info@tommytrinder.com" class="button button--primary">Contact Us</a>
             </header>
         </div>
-        
-
-       
+      
          <div class="main-container">
             <div class="main clearfix">
